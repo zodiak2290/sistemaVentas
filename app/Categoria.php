29416afd40a26b29e12a3a3991edc4e38f27e053
Alberto <albon_marvel@hotmail.com>
2016-08-10 11:02:51 -0500

    Espefificando atributos

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $table="categoria";
+
+    protected $primaryKey="idcategoria";
+
+    public $timestamps=false;
+
+    protected $fillable=[
+    'nombre',
+    'descripcion',
+    'condicion'];
+
+    protected $guarded=[];
 }
