825763a0ed7e9943c2c8b9f9dbd69197c5709607
Alberto <albon_marvel@hotmail.com>
2016-08-10 11:56:58 -0500

    Cargando archivo de stylos y js

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Sistema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Sistema\Http\Requests;
+use Sistema\app\Categoria.php;
+use Illuminate\Support\Facades\Redirect;
+use Sistema\Http\Requests\CategoriaFormRequest;
+class CategoriaController extends Controller
+{
+	public function __construct()
+	{
+
+	}
+
+	public function index(Request $request)
+	{
+		if($request)
+		{
+			$query=trim($request->get('searchText'));
+			$categorias=DB::table('categoria')->where('nombre','LIKE',"%".$query."%")
+							->where('condicion',"=",'1')
+							->orderBy('idcategoria',"desc")
+							->paginate(7);
+			return view('almacen.categoria.index',["categorias"=>$categorias,"searchText"=>$query]);
+		}
+
+	}
+
+	public function create()
+	{
+		return view("almacen.categoria.create");
+	}
+
+	public function store(CategoriaFormRequest $request)
+	{
+		$categoria=new Categoria;
+		$categoria->nombre=$request->get('nombre');
+		$categoria->descripcion=$request->get('descripcion');
+		$categoria->condicion='1';
+		$categoria->save();
+		return Redirect::to('almacen/categoria');
+	}
+
+	public function show($id)
+	{	
+		return view("almacen.categoria.show",["categoria"=>Categoria::findOrFail($id)]);
+	}
+
+	public function edit($id)
+	{
+		return view("almacen.categoria.edit",["categoria"=>Categoria::findOrFail($id)]);
+	}
+
+	public function update(CategoriaFormRequest $request,$id)
+	{
+		$categoria=Categoria::findOrFail($id);
+		$categoria->nombre=$request->get('nombre');
+		$categoria->descripcion=$request->get('descripcion');
+		$categoria->update();
+		return Redirect::to('almacen/categoria');
+
+	}
+
+	public function destroy($id)
+	{
+		$categoria=Categoria::findOrFail($id);
+		$categoria->condicion='0';
+		$categoria->update();
+		return Redirect::to('almacen/categoria');
+	}
+    //
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resourse('almacen/categoria','CategoriaController');
+Route::resource('almacen/categoria','CategoriaController');
